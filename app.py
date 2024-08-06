7059dc8cfde5f33777724492eb3a7396c76c6196
Julio Balbino <julio4110@hotmail.com>
2024-08-06 19:26:18 -0300

    feat: take music list from file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory, abort,send_file
+from flask import Flask, render_template, request, send_from_directory, abort, send_file
 from flask_socketio import SocketIO, emit
 from udp_sender import UDPSender
 from qrcodeaux import generate_qr_code
@@ -6,7 +6,7 @@ import os
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
 import parameters as pm
-
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -53,9 +53,16 @@ def handle_message(message):
     emit('response', {'data': 'Mensagem recebida!'})
 
 
-@app.route('/send_music', methods=['GET'])
-def test_udp():
-    message = "start Evanescence-Bring-Me-to-Life"
+@app.route('/send_music_test', methods=['GET'])
+def send_music_test():
+    message = "start Fall-Out-Boy-Sugar-We_re-Goin-Down"
+    udp_sender.send(message)
+    return "Message sent!"
+
+
+@app.route('/send_music', methods=['POST'])
+def send_music():
+    message = "start " + request.form["music"]
     udp_sender.send(message)
     return "Message sent!"
 
@@ -66,15 +73,40 @@ def test_html():
     return "Render music-list.html em todos os clientes!"
 
 
-@app.route('/music-list.html')
+@app.route('/music-list')
 def render_test_html():
-    return render_template('music-list.html')
+    musics = []
+    try:
+        with open('static/music_list/musics.txt', 'r') as arquivo:
+            for linha in arquivo:
+                musics.append(linha.strip())
+    except FileNotFoundError:
+        return None
+    print(musics)
+
+    return render_template('music-list.html', musics=musics)
 
 
-@app.route('/qr/<video_id>')
-def get_qrcode_score(video_id):
-    qr_image = generate_qr_code(pm.BASE_URL + '/download_video_page/' + video_id + '.mp4')
+@app.route('/qr')
+def get_qrcode_score():
+    video_id = get_most_recent_file()
+    qr_image = generate_qr_code(pm.BASE_URL + '/download_video_page/' + video_id)
     return send_file(qr_image, mimetype='image/png')
+
+
+def get_most_recent_file():
+    directory = 'static/videos'
+    if not os.path.isdir(directory):
+        raise FileNotFoundError(f"O diretório {directory} não foi encontrado.")
+
+    files = [f for f in Path(directory).iterdir() if f.is_file()]
+
+    if not files:
+        raise FileNotFoundError("Não há arquivos no diretório.")
+
+    most_recent_file = max(files, key=os.path.getmtime)
+
+    return most_recent_file.name
 
 
 @app.route('/download_video_page/<filename>')
